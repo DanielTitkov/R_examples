6577b4ff9c26ed1b57d72db2b5dd4bb8cdf1c875
Daniel <mytasmic@gmail.com>
2018-02-06 23:30:21 +0300

    added some bugs

--- graph.R
+++ graph.R
@@ -4,9 +4,24 @@ library(tidyverse)
 df <- read_csv('data.csv')
 glimpse(df)
 
+
+# grouped barplot
 df %>%
   ggplot(aes(v.elision, fill = position)) +
   geom_bar(position = 'dodge') + 
-  labs(title = 'Coorelation between [v] elision and the type of pharal position',
+  labs(title = 'Correlation between [v] elision and the type of phrasal position',
        y='number of cases')
+
+# dotplot for crosstab
+df %>%
+  group_by(v.elision, position) %>%
+  summarise(number = n()) %>%
+  ggplot(aes(v.elision, position, label = number)) + 
+  geom_point(aes(size=number), color='lightblue') +
+  scale_size(range = c(10, 30)) +
+  guides(size = F) +
+  geom_text() + 
+  labs(title = 'Correlation between [v] elision and the type of phrasal position',
+       y='phrasal position', x='[v] elision')
+
 
